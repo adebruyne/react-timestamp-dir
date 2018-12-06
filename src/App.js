9d1907267eb258f0ffd7c3425f307a42755c1190
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-06 09:31:52 -0500

    now, we refactor a little by mapping over anm array we wrote

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const someDates = ["2018-12-01", "2018-12-4"];
+
 class App extends Component {
   render() {
+    const dateAsListItems = someDates.map(dateString => {
+      return <li>{dateString}</li>;
+    });
+
     return (
       <div className="App">
         <div className="main-component">
           <button className="my-button">go ahead, click me. i dare you</button>
-          <ul className="date-list">
-            <li>Date 1</li>
-            <li>Date 2</li>
-          </ul>
+          <ul className="date-list">{dateAsListItems}</ul>
         </div>
       </div>
     );
